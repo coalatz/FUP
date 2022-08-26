7fea8b87c402fc76a9c785a17a312f7db9c70421
coalatz <104813706+coalatz@users.noreply.github.com>
2022-08-26 17:48:32 -0300

    adicionada tarefa

---
+++ mediaAritmetica.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    float var1, var2;
+    float soma=0;
+    float var3 =0;
+    
+    scanf("%f",&var1);
+    scanf("%f",&var2);
+    
+    soma = (var1 + var2)/2;
+    
+    
+    printf("%.2f", soma);
+    
+}
